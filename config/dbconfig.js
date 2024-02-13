3d972f65fef54ce4d7833c4837bfa504f81cbd91
Rahhul1309 <abbavaram.r@northeastern.edu>
2024-02-12 19:05:02 -0500

    added the secrets

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,20 +1,24 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 //require('dotenv').config()
 
-const sequelize = new Sequelize(secrets.DB_FILE,{
-  define : {
-    freezeTableName : true
-  }
-})
+const sequelize = new Sequelize({
+  dialect: "postgres",
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: "postgres", // Replace 'your_database_name' with your actual database name
+  host: "localhost", // You can change the host if your database is hosted elsewhere
+  define: {
+    freezeTableName: true,
+  },
+});
 
-
-async function testConnection(){
-try {
-   const resp =  await sequelize.authenticate();
-    return true
+async function testConnection() {
+  try {
+    const resp = await sequelize.authenticate();
+    return true;
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
-    return false
+    console.error("Unable to connect to the database:", error);
+    return false;
   }
 }
-  module.exports = {testConnection,sequelize};
+module.exports = { testConnection, sequelize };
