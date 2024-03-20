2d437360151efd689e140733f3350bc6cc69b483
Rahhul1309 <abbavaram.r@northeastern.edu>
2024-03-20 13:39:39 -0400

    added the code

--- logger.js
+++ logger.js
@@ -1,37 +1,13 @@
 const winston = require('winston');
 
-const customLevels = {
-  levels: {
-    error: 0,
-    warn: 1,
-    info: 2,
-    debug: 3
-  },
-  colors: {
-    error: 'red',
-    warn: 'yellow',
-    info: 'green',
-    debug: 'blue'
-  }
-};
 
-winston.addColors(customLevels.colors);
+
 
 const logger = winston.createLogger({
   level: 'info',
-  levels: customLevels.levels,
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.printf(info => {
-      const { timestamp, level, message, ...args } = info;
-      const logObject = {
-        timestamp,
-        severity: level.toUpperCase(),
-        message: message,
-        ...args // Include additional properties
-      };
-      return JSON.stringify(logObject); // Return the log object as JSON string
-    })
+    winston.format.json(),
   ),
   transports: [
     new winston.transports.File({ filename: 'logfile.log' })
