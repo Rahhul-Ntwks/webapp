a233e2fa7d8ce2536525b96e19482bf1bf22f8bb
Rahhul1309 <abbavaram.r@northeastern.edu>
2024-03-20 00:48:03 -0400

    added the code

--- install_ops_agent.sh
+++ install_ops_agent.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install Ops Agent (replace this with your actual installation steps)
-curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
-sudo bash add-monitoring-agent-repo.sh --also-install
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 cp  /home/csye6225/webapp/logger.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
